f504883392defc88400abc384009c6105543b39d
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-07-05 22:01:32 +0300

    Data api endpoint design

--- client/src/Projects/Projects.js
+++ client/src/Projects/Projects.js
@@ -1,6 +1,65 @@
-import React from 'react'
+import React from 'react';
+import "./projects.css";
 
 const Projects = () => {
+
+    const projectList = [
+        {
+            name:"MERN Stack Job Portal",
+            des:"This project is to publish job for the employer and apply job for smployees",
+            tech1:"Node JS",
+            tech2: "React JS",
+            tech3:"MogoDB",
+            tech4:"Expres JS",
+            projectlink:"https://react-company-profile.netlify.app/"
+        },
+        {
+            name:"MERN Stack Event Management",
+            des:"This project is to publish job for the employer and apply job for smployees",
+            tech1:"Node JS",
+            tech2: "React JS",
+            tech3:"MogoDB",
+            tech4:"Expres JS",
+            projectlink:"https://react-company-profile.netlify.app/"
+        },
+        {
+            name:"MERN Stack Event Management",
+            des:"This project is to publish job for the employer and apply job for smployees",
+            tech1:"Node JS",
+            tech2: "React JS",
+            tech3:"MogoDB",
+            tech4:"Expres JS",
+            projectlink:"https://react-company-profile.netlify.app/"
+        },
+        {
+            name:"MERN Stack Event Management",
+            des:"This project is to publish job for the employer and apply job for smployees",
+            tech1:"Node JS",
+            tech2: "React JS",
+            tech3:"MogoDB",
+            tech4:"Expres JS",
+            projectlink:"https://react-company-profile.netlify.app/"
+        },
+        {
+            name:"MERN Stack Event Management",
+            des:"This project is to publish job for the employer and apply job for smployees",
+            tech1:"Node JS",
+            tech2: "React JS",
+            tech3:"MogoDB",
+            tech4:"Expres JS",
+            projectlink:"https://react-company-profile.netlify.app/"
+        },
+        {
+            name:"MERN Stack Event Management",
+            des:"This project is to publish job for the employer and apply job for smployees",
+            tech1:"Node JS",
+            tech2: "React JS",
+            tech3:"MogoDB",
+            tech4:"Expres JS",
+            projectlink:"https://react-company-profile.netlify.app/"
+        },
+    ]
+
   return (
     <div>Projects</div>
   )
