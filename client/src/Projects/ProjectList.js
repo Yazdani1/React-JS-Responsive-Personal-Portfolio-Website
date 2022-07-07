198a764a9abe8c2e7023b3cd6bb61aba64a6b948
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-07-07 21:17:44 +0300

    Project list design

--- client/src/Projects/ProjectList.js
+++ client/src/Projects/ProjectList.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./projects.css"
+
 
 const ProjectList = ({name,des,tech1,tech2,tech3,tech4,projectlink,index}) => {
+
+  const [show,setShow] = useState(false);
+
   return (
     <div className='project-list'>
         
