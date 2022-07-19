e5d975800668d6deaeaa3da5ed763a34489f2f0d
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-07-19 23:22:01 +0300

    cleaning code

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -27,69 +27,17 @@ const About = () => {
                 <span className="line"></span>
               </div>
 
-              <p>
-                It is a long established fact that a reader will be distracted
-                by the readable content of a page when looking at its layout.
-                The point of using Lorem Ipsum is that it has a more-or-less
-                normal distribution of letters, as opposed to using 'Content
-                here, content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem
-                Ipsum as their default model text, and a search for 'lorem
-                ipsum' will uncover many web sites still in their infancy.
-                Various versions have evolved over the years, sometimes by
-                accident, sometimes on purpose (injected humour and the like).
-              </p>
+        
 
-              {/* <div className="card-design">
-                <div className="about-list-item">
-                  <div className="about-details-icon">
                     <p>
-                      <FcSlrBackSide size={50} />
+                      It is a long established fact that a reader will be distracted
+                      by the readable content of a page when looking at its layout.
+                      The point of using Lorem Ipsum is that it has a more-or-less
+                      normal distribution of letters, as opposed to using 'Content
+                      here, content here', making it look like readable English. 
                     </p>
-                  </div>
-                  <div>
-                    <h6>Perfect day out</h6>
-                    <p>
-                      It is a long established fact that a reader will be
-                      distracted by the readable content.
-                    </p>
-                  </div>
-                </div>
-              </div>
+           
 
-              <div className="card-design">
-                <div className="about-list-item">
-                  <div className="about-details-icon">
-                    <p>
-                      <FcWorkflow size={50} />
-                    </p>
-                  </div>
-                  <div>
-                    <h6>Perfect day out</h6>
-                    <p>
-                      It is a long established fact that a reader will be
-                      distracted by the readable content.
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="card-design">
-                <div className="about-list-item">
-                  <div className="about-details-icon">
-                    <p>
-                      <FcShop size={50} />
-                    </p>
-                  </div>
-                  <div>
-                    <h6>Perfect day out</h6>
-                    <p>
-                      It is a long established fact that a reader will be
-                      distracted by the readable content.
-                    </p>
-                  </div>
-                </div>
-              </div> */}
             </div>
           </div>
         </Fade>
