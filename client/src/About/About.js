2b84c489a211630cc80be92416f3aa9b87f09bc8
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-07-19 23:23:22 +0300

    adding animation

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -27,17 +27,15 @@ const About = () => {
                 <span className="line"></span>
               </div>
 
-        
-
-                    <p>
-                      It is a long established fact that a reader will be distracted
-                      by the readable content of a page when looking at its layout.
-                      The point of using Lorem Ipsum is that it has a more-or-less
-                      normal distribution of letters, as opposed to using 'Content
-                      here, content here', making it look like readable English. 
-                    </p>
-           
-
+              <Fade left>
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.
+                  The point of using Lorem Ipsum is that it has a more-or-less
+                  normal distribution of letters, as opposed to using 'Content
+                  here, content here', making it look like readable English.
+                </p>
+              </Fade>
             </div>
           </div>
         </Fade>
